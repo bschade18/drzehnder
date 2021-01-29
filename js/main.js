12885ee10e9a6413589e93e92a287ac4eb6aefe6
Bobby Schade <bschade.18@gmail.com>
2021-01-29 16:23:52 -0600

    added bootstrap nav and expandable menu for mobile links

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// function myFunction() {
+//   const mobileTabs = document.querySelector('.navbar-tabs-mobile');
+
+//   if (mobileTabs.style.display === 'block') {
+//     mobileTabs.style.display = 'none';
+//   } else {
+//     mobileTabs.style.display = 'block';
+//   }
+// }
+
+// showMoreBtn.addEventListener('click', () => {
+//   showMore.classList.toggle('view');
+
+//   if (showMoreBtn.innerHTML === 'Show Smaller Apps') {
+//     showMoreBtn.innerHTML = 'Show Less';
+//   } else {
+//     showMoreBtn.innerHTML = 'Show Smaller Apps';
+//   }
+// });
